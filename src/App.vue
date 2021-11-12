a05680a736937532cea77cd5fd90929d23f3d54b
evgenii.ushakov <evgenii.ushakov@alertgroup.ru>
2021-11-12 14:03:03 +0300

    standart options for webpack

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<!--App.vue-->
+<template>
+    <div>Hello, X1AXX1A</div>
+</template>
+<script>
+import { defineComponent } from "vue"
+export default defineComponent({
+  setup() {
+      console.log('I am App Printing in the library')
+  }  
+})
+</script>
