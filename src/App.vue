ba52f1212f33abb3177c7294f98b9c9c82179494
evgenii.ushakov <evgenii.ushakov@alertgroup.ru>
2021-11-12 14:11:52 +0300

    custom vue installing - vue3, vue-router, vuex

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,18 @@
 </template>
 <script>
 import { defineComponent } from "vue"
+import { useStore } from 'vuex'
+import { useRouter, useRoute } from 'vue-router'
+
 export default defineComponent({
   setup() {
-      console.log('I am App Printing in the library')
+    let store = useStore()
+    let router = useRouter()
+    let route = useRoute()
+    console.log('store', store)
+    console.log('router', router)
+    console.log('route', route)
+    console.log('I am App The printing of 123112')
   }  
 })
 </script>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import App from './App.vue'
 import { createApp } from 'vue'
+import store from './store'
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
